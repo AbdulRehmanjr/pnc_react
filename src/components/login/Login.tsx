9b510c6c17f9a40aabad695a0863efcac34070d0
AbdulRehmanjr <abdulrehman2020white@gamil.com>
2023-09-10 21:01:01 +0500

    form hook working

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -6,23 +6,24 @@ function Login() {
     <section className="vh-100">
     <div className="container-fluid h-custom">
       <div className="row d-flex justify-content-center align-items-center h-100">
-        <div className="col-md-9 col-lg-6 col-xl-5">
-          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+        <div className="d-none d-md-block col-md-9 col-lg-6 col-xl-5">
+          <img src="src/assets/login.jpeg"
             className="img-fluid" alt="Sample image" />
         </div>
-        <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+        <div className="col-sm-12 col-md-8 col-lg-6 col-xl-4 offset-xl-1">
           <form>
+            <h1>Login Form</h1>
             <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
               <p className="lead fw-normal mb-0 me-3">Sign in with</p>
-              <button type="button" className="btn btn-primary btn-floating mx-1">
+              <button type="button" className="btn btn-success btn-floating mx-1">
                 <i className="fab fa-facebook-f"></i>
               </button>
   
-              <button type="button" className="btn btn-primary btn-floating mx-1">
+              <button type="button" className="btn btn-success btn-floating mx-1">
                 <i className="fab fa-twitter"></i>
               </button>
   
-              <button type="button" className="btn btn-primary btn-floating mx-1">
+              <button type="button" className="btn btn-success btn-floating mx-1">
                 <i className="fab fa-linkedin-in"></i>
               </button>
             </div>
@@ -57,7 +58,7 @@ function Login() {
             </div>
   
             <div className="text-center text-lg-start mt-4 pt-2">
-              <button type="button" className="btn btn-primary btn-lg"
+              <button type="button" className="btn btn-success btn-lg"
                 >Login</button>
               <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
                   className="link-danger">Register</a></p>
--- src/components/login/Signup.tsx
+++ src/components/login/Signup.tsx
@@ -1,90 +1,164 @@
 import './Signup.css'
 
+import { useForm, Controller } from 'react-hook-form';
+
+import * as yup from 'yup';
+
 function Signup() {
+
+
+    const { handleSubmit, control, formState: { errors } } = useForm();
+
+    // Create a yup schema for validation
+    const schema = yup.object().shape({
+        firstName: yup.string().required('First name is required'),
+        lastName: yup.string().required('Last name is required'),
+        email: yup.string().email('Invalid email address').required('Email is required'),
+        password: yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
+    });
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+    console.log('rending')
     return (
 
-            <section className="login_bg_color">
-                <div className="px-4 py-4 px-md-5 text-center text-lg-start" >
-                    <div className="container">
-                        <div className="row gx-lg-5 align-items-center">
-                            <div className="col-lg-6">
-                                <h1 className="my-4 display-3 fw-bold ls-tight text_green">
-                                    The best offer <br />
-                                    <span className="text-white">htmlFor your business</span>
-                                </h1>
-                                <p >
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                    Eveniet, itaque accusantium odio, soluta, corrupti aliquam
-                                    quibusdam tempora at cupiditate quis eum maiores libero
-                                    veritatis? Dicta facilis sint aliquid ipsum atque?
-                                </p>
-                            </div>
+        <section className="login_bg_color">
+            <div className="px-4 py-4 px-md-5 text-center text-lg-start" >
+                <div className="container">
+                    <div className="row gx-lg-5 align-items-center">
+                        <div className="col-lg-6">
+                            <h1 className="my-4 display-3 fw-bold ls-tight text_green">
+                                The best offer <br />
+                                <span className="text-white">htmlFor your business</span>
+                            </h1>
+                            <p >
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Eveniet, itaque accusantium odio, soluta, corrupti aliquam
+                                quibusdam tempora at cupiditate quis eum maiores libero
+                                veritatis? Dicta facilis sint aliquid ipsum atque?
+                            </p>
+                        </div>
 
-                            <div className="col-lg-6">
-                                <div className="card">
-                                    <div className="card-body py-4 px-md-5">
-                                        <form>
-
-                                            <div className="row">
-                                                <div className="col-md-6 mb-4">
-                                                    <div className="form-outline">
-                                                        <input type="text" id="form3Example1" className="form-control" />
-                                                        <label className="form-label" htmlFor="form3Example1">First name</label>
-                                                    </div>
+                        <div className="col-lg-6">
+                            <div className="card">
+                                <div className="card-body py-4 px-md-5">
+                                    <form onSubmit={handleSubmit(onSubmit)} >
+                                        <div className="row">
+                                            <div className="col-md-6 mb-4">
+                                                <div className="form-outline">
+                                                    <Controller
+                                                        name="firstName"
+                                                        control={control}
+                                                        defaultValue=""
+                                                        render={({ field }) => (
+                                                            <div>
+                                                                <input
+                                                                    type="text"
+                                                                    className={`form-control ${errors.firstName ? 'is-invalid' : ''}`}
+                                                                    placeholder="First name"
+                                                                    {...field}
+                                                                />
+                                                                {errors.firstName && <div className="invalid-feedback">{errors.firstName.message}</div>}
+                                                            </div>
+                                                        )}
+                                                    />
                                                 </div>
-                                                <div className="col-md-6 mb-4">
-                                                    <div className="form-outline">
-                                                        <input type="text" id="form3Example2" className="form-control" />
-                                                        <label className="form-label" htmlFor="form3Example2">Last name</label>
-                                                    </div>
+                                            </div>
+                                            <div className="col-md-6 mb-4">
+                                                <div className="form-outline">
+                                                    <Controller
+                                                        name="lastName"
+                                                        control={control}
+                                                        defaultValue=""
+                                                        render={({ field }) => (
+                                                            <div>
+                                                                <input
+                                                                    type="text"
+                                                                    className={`form-control ${errors.lastName ? 'is-invalid' : ''}`}
+                                                                    placeholder="Last name"
+                                                                    {...field}
+                                                                />
+                                                                {errors.lastName && <div className="invalid-feedback">{errors.lastName.message}</div>}
+                                                            </div>
+                                                        )}
+                                                    />
                                                 </div>
                                             </div>
+                                        </div>
 
+                                        <div className="form-outline mb-4">
+                                            <Controller
+                                                name="email"
+                                                control={control}
+                                                defaultValue=""
+                                                render={({ field }) => (
+                                                    <div>
+                                                        <input
+                                                            type="email"
+                                                            className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                                                            placeholder="Email address"
+                                                            {...field}
+                                                        />
+                                                        {errors.email && <div className="invalid-feedback">{errors.email.message}</div>}
+                                                    </div>
+                                                )}
+                                            />
+                                        </div>
 
-                                            <div className="form-outline mb-4">
-                                                <input type="email" id="form3Example3" className="form-control" />
-                                                <label className="form-label" htmlFor="form3Example3">Email address</label>
-                                            </div>
+                                        <div className="form-outline mb-4">
+                                            <Controller
+                                                name="password"
+                                                control={control}
+                                                defaultValue=""
+                                                render={({ field }) => (
+                                                    <div>
+                                                        <input
+                                                            type="password"
+                                                            className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+                                                            placeholder="Password"
+                                                            {...field}
+                                                        />
+                                                        {errors.password && <div className="invalid-feedback">{errors.password.message}</div>}
+                                                    </div>
+                                                )}
+                                            />
+                                        </div>
 
+                                        <button type="submit" className="btn btn-success btn-block mb-4 shadow">
+                                            Sign up
+                                        </button>
 
-                                            <div className="form-outline mb-4">
-                                                <input type="password" id="form3Example4" className="form-control" />
-                                                <label className="form-label" htmlFor="form3Example4">Password</label>
-                                            </div>
-
-                                            <button type="submit" className="btn btn-success btn-block mb-4 shadow">
-                                                Sign up
+                                        <hr />
+                                        <div className="text-center">
+                                            <p>or sign up with:</p>
+                                            <button type="button" className="btn btn-link btn-floating mx-1">
+                                                <i className="fab fa-facebook-f login_icon"></i>
                                             </button>
 
-                                            <hr />
-                                            <div className="text-center">
-                                                <p>or sign up with:</p>
-                                                <button type="button" className="btn btn-link btn-floating mx-1">
-                                                    <i className="fab fa-facebook-f login_icon"></i>
-                                                </button>
+                                            <button type="button" className="btn btn-link btn-floating mx-1">
+                                                <i className="fab fa-google login_icon"></i>
+                                            </button>
 
-                                                <button type="button" className="btn btn-link btn-floating mx-1">
-                                                    <i className="fab fa-google login_icon"></i>
-                                                </button>
+                                            <button type="button" className="btn btn-link btn-floating mx-1">
+                                                <i className="fab fa-twitter login_icon"></i>
+                                            </button>
 
-                                                <button type="button" className="btn btn-link btn-floating mx-1">
-                                                    <i className="fab fa-twitter login_icon"></i>
-                                                </button>
+                                            <button type="button" className="btn btn-link btn-floating mx-1">
+                                                <i className="fab fa-github login_icon"></i>
+                                            </button>
 
-                                                <button type="button" className="btn btn-link btn-floating mx-1">
-                                                    <i className="fab fa-github login_icon"></i>
-                                                </button>
-                                            </div>
-                                        </form>
-                                    </div>
+                                        </div>
+                                    </ form>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </section>
-        
+            </div>
+        </section>
+
     )
 }
 
 export default Signup
+
